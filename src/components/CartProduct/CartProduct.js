3c34e7d4ee41d0bb540741ca46f75a78adf32926
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-26 09:56:26 +0600

    Add font awsome and use some simple icon

--- src/components/CartProduct/CartProduct.js
+++ src/components/CartProduct/CartProduct.js
@@ -1,3 +1,5 @@
+import { faRemove } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const CartProduct = ({cartProduct}) => {
@@ -9,11 +11,13 @@ const CartProduct = ({cartProduct}) => {
             <div className='col-2 d-none d-lg-block'>
                 <img src={img} alt="" width="30"/>
             </div>
-            <div className='col-8'>
+            <div className='col-9 col-sm-8'>
                 <p className='text-white'>{name}</p>
             </div>
-            <div className='col-2'>
-                <button className='btn btn-dark'>X</button>
+            <div className='col-3 col-sm-2'>
+                <button className='btn btn-dark'>
+                    <FontAwesomeIcon icon={faRemove}></FontAwesomeIcon>
+                </button>
             </div>
         </div>
     );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Product = ({product, handleClick}) => {
     const {id, img, name, price} = product;
@@ -11,7 +13,7 @@ const Product = ({product, handleClick}) => {
                     <h6 className="card-text">Price : {price}</h6>
                 </div>
                 <div className="card-footer">
-                    <button className='btn btn-primary w-100 fw-bold' onClick={() => handleClick(id)}>Add to cart</button>
+                    <button className='btn btn-primary w-100 fw-bold' onClick={() => handleClick(id)}>Add to cart <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon> </button>
                 </div>
             </div>
         </div>
