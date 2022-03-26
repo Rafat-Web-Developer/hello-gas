1ea48c784eec498fa0a3d81dbfcbf59c4051f414
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-26 09:15:17 +0600

    Create a new Cart Product component for showing single cart product

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import CartProduct from '../CartProduct/CartProduct';
 
 const Cart = ({cartProducts}) => {
-    console.log(cartProducts);
 
     let count = 0;
 
@@ -9,6 +9,11 @@ const Cart = ({cartProducts}) => {
         <div className='mt-3 sticky-top'>
             <h3 className='text-center text-white mb-4'>Your Cart</h3>
             {count === 0 && <h5 className='text-center'>Empty cart</h5>}
+            <div>
+                {
+                    cartProducts.map(product => <CartProduct key={product.id}></CartProduct>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/CartProduct/CartProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartProduct = () => {
+    return (
+        <div>
+            <h3>Cart Product</h3>
+        </div>
+    );
+};
+
+export default CartProduct;
